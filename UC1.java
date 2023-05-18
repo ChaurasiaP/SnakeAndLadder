d8896050ed89b23188c108118ecff00f46999180
ChaurasiaP <pranshu1431@gmail.com>
2023-05-18 21:53:28 +0530

    ADDED UC1

---
+++ UC1.java
@@ -0,0 +1,9 @@
+package UseCases;
+
+public class UC1 {
+    public static void main(String[] args) {
+        int startPos=0;
+        System.out.println("Game Starts!");
+        System.out.println("Player starting position: "+startPos);
+    }
+}
